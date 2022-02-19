c3ee62469dda1356889b04fbca73ba7ffeacec5d
trungthach020201 <wendythach489@gmail.com>
2022-02-19 14:16:23 +0700

    User tem

---
+++ FPTBOOKSTORE_TEMPLETE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FPTBOOKSTORE_TEMPLETE.Startup))]
+namespace FPTBOOKSTORE_TEMPLETE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
